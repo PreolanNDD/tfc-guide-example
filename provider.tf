af25d49160d5df5819ae52c31f0d408c61c15cc2
Preolan Naidu <Preolan@domaindimensions.co.za>
2025-01-09 11:37:44 +0200

    added aft provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = "us-east-1" # Replace with your desired AWS region
 }
+
+provider "aws" {
+  alias  = "aft_management"
+  region = "us-east-1" # Replace with the region where the resource was created
+}
+
