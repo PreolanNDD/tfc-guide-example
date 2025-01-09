fe04a3153fcb31cf03a714655ffcbcdcaf2f9f0e
Preolan Naidu <Preolan@domaindimensions.co.za>
2025-01-09 16:32:31 +0200

    testing tf provisioning

--- main.tf
+++ main.tf
@@ -1,63 +1,29 @@
-resource "aws_s3_bucket" "account_creation_bucket" {
-  bucket = "account-creation-bucket1"
-}
-
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.account_creation_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+resource "aws_servicecatalog_provisioned_product" "account" {
+  name = "new-account"
+  product_id = "prod-aw6rmaq7plaea"
+  provisioning_artifact_id = "pa-uleitpzbpslcq"
+  provisioning_parameters {
+        key = "AccountName"
+        value = "Test-Account"
+  }
+  provisioning_parameters {
+        key = "AccountEmail"
+        value = "test@domaindimensions.co.za"    
+  }
+  provisioning_parameters {
+        key = "SSOUserFirstName"
+        value = "Test"
+  }
+  provisioning_parameters {
+        key = "SSOUserLastName"
+        value = "testlast"    
+  }
+    provisioning_parameters {
+        key = "SSOUserEmail"
+        value = "test@domaindimensions.co.za"
+  }
+  provisioning_parameters {
+        key = "ManagedOrganizationalUnit"
+        value = "Sandbox (ou-qaok-zpp1cwpv)"    
   }
 }
-
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
-  bucket = aws_s3_bucket.account_creation_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_object" "cf_template" {
-  bucket = aws_s3_bucket.account_creation_bucket.id
-  key    = "cloudformation/account-creation-template.yaml"
-  source = "account-creation-template.yaml"
-  acl    = "private"
-}
-
-resource "aws_servicecatalog_product" "custom_product" {
-  name          = "Account Creation Product"
-  owner         = "YourCompanyName"
-  type          = "CLOUD_FORMATION_TEMPLATE"
-
-  provisioning_artifact_parameters {
-    name                   = "v1"
-    description            = "Template to create AWS accounts"
-    type                   = "CLOUD_FORMATION_TEMPLATE"
-    template_url           = "https://s3.amazonaws.com/account-creation-bucket1/cloudformation/account-creation-template.yaml"
-  }
-  distributor   = "DD"
-  description   = "Product for creating AWS accounts"
-
-  lifecycle {
-    # Ensure Terraform detects changes to the CloudFormation template
-    # and updates the product by forcing a new deployment
-    create_before_destroy = true
-  }
-}
-
-resource "aws_servicecatalog_portfolio" "custom_portfolio" {
-  name          = "Custom Account Portfolio"
-  description   = "Portfolio for provisioning AWS accounts"
-  provider_name = "DD"
-}
-
-resource "aws_servicecatalog_product_portfolio_association" "example" {
-  portfolio_id = aws_servicecatalog_portfolio.custom_portfolio.id
-  product_id   = aws_servicecatalog_product.custom_product.id
-}
-
-resource "aws_servicecatalog_principal_portfolio_association" "example" {
-  portfolio_id = aws_servicecatalog_portfolio.custom_portfolio.id
-  principal_arn = "arn:aws:iam::194722434270:user/Preolan"
-}
-
-
---
+++ working/main.tf
@@ -0,0 +1,63 @@
+resource "aws_s3_bucket" "account_creation_bucket" {
+  bucket = "account-creation-bucket1"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.account_creation_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.account_creation_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_object" "cf_template" {
+  bucket = aws_s3_bucket.account_creation_bucket.id
+  key    = "cloudformation/account-creation-template.yaml"
+  source = "account-creation-template.yaml"
+  acl    = "private"
+}
+
+resource "aws_servicecatalog_product" "custom_product" {
+  name          = "Account Creation Product"
+  owner         = "YourCompanyName"
+  type          = "CLOUD_FORMATION_TEMPLATE"
+
+  provisioning_artifact_parameters {
+    name                   = "v1"
+    description            = "Template to create AWS accounts"
+    type                   = "CLOUD_FORMATION_TEMPLATE"
+    template_url           = "https://s3.amazonaws.com/account-creation-bucket1/cloudformation/account-creation-template.yaml"
+  }
+  distributor   = "DD"
+  description   = "Product for creating AWS accounts"
+
+  lifecycle {
+    # Ensure Terraform detects changes to the CloudFormation template
+    # and updates the product by forcing a new deployment
+    create_before_destroy = true
+  }
+}
+
+resource "aws_servicecatalog_portfolio" "custom_portfolio" {
+  name          = "Custom Account Portfolio"
+  description   = "Portfolio for provisioning AWS accounts"
+  provider_name = "DD"
+}
+
+resource "aws_servicecatalog_product_portfolio_association" "example" {
+  portfolio_id = aws_servicecatalog_portfolio.custom_portfolio.id
+  product_id   = aws_servicecatalog_product.custom_product.id
+}
+
+resource "aws_servicecatalog_principal_portfolio_association" "example" {
+  portfolio_id = aws_servicecatalog_portfolio.custom_portfolio.id
+  principal_arn = "arn:aws:iam::194722434270:user/Preolan"
+}
+
+
